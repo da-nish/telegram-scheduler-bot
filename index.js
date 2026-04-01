392a45771e546adbb4d544fd630216734433bd37
danish khan <danishdev64@gmail.com>
2026-04-01 18:11:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const cron = require('node-cron');
+
+
+// dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const BOT_TOKEN = '8';
+const CHAT_ID = '1';
+
+async function sendTelegramMessage() {
+
+    const fetch = (await import('node-fetch')).default;
+    const message = 'Test message from MacBook Pro!';
+
+    console.log(message)
+    try {
+        const response = await fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ chat_id: CHAT_ID, text: message })
+        });
+        const data = await response.json();
+        console.log('Telegram Response:', data);
+    } catch (error) {
+        console.error('Error sending Telegram message:', error);
+    }
+}
+
+// API endpoint
+app.get('/api/test_app_telegram', async (req, res) => {
+  try {
+    await sendTelegramMessage();
+    res.json({ success: true, message: 'Message sent!' });
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// Schedule task 3 times a day at 9:00, 14:00, 20:00
+// cron.schedule('0 9,14,20 * * *', () => {
+//   console.log('Running scheduled Telegram message...');
+//   sendTelegramMessage();
+// });
+
+cron.schedule('* * * * *', () => {
+  console.log('Running scheduled Telegram message...');
+  sendTelegramMessage();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
